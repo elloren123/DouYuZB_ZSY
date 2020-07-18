f95015a09682128970c452b41f90d321a21be7a7
ZSY <elloren123@163.com>
2020-07-18 16:37:14 +0800

    设置颜值cell

--- DouYuZhiBo_new/DouYuZhiBo_new/Classes/Home/Controller/RecommendViewController.swift
+++ DouYuZhiBo_new/DouYuZhiBo_new/Classes/Home/Controller/RecommendViewController.swift
@@ -12,10 +12,11 @@ import UIKit
 
 private let kItemMargin : CGFloat = 10
 private let kItemW : CGFloat = (kScreenW - kItemMargin * 3)/2
-private let kItemH : CGFloat = kItemW * 3 / 4
+private let kNormalItemH : CGFloat = kItemW * 3 / 4
+private let kBueautifulItemH : CGFloat = kItemW * 4 / 3
 private let kHeaderH : CGFloat = 50
 private let kNormalCellID = "RecommendVCkNormalCellID"
-
+private let kBueautifulCellID = "RecommendVCkBueautifulCellID"
 private let kNormalHeaderID = "RecommendVCkNormalHeaderID"
 
 class RecommendViewController: UIViewController {
@@ -23,7 +24,7 @@ class RecommendViewController: UIViewController {
     private lazy var collectionView:UICollectionView = { [unowned self] in
         //创建布局
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+        layout.itemSize = CGSize(width: kItemW, height: kNormalItemH)
         layout.minimumLineSpacing = 1 //应该给0,给你是方便查看
         layout.minimumInteritemSpacing = kItemMargin //这里设置一个最新内边距 10, 实际上,这里会被分配成30,所以要加内边距
         layout.sectionInset = UIEdgeInsets(top: 0, left: kItemMargin, bottom: 0, right: kItemMargin)
@@ -38,6 +39,7 @@ class RecommendViewController: UIViewController {
         let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
         collectionView.backgroundColor = UIColor.white
         collectionView.dataSource = self
+        collectionView.delegate = self
         //给collectionView添加约束,让其随着父控件拉伸
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         
@@ -45,7 +47,7 @@ class RecommendViewController: UIViewController {
 //        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kNormalCellID)
         //注册普通cell-->XIB
         collectionView.register(UINib(nibName: "CollectionViewNormalCell", bundle: nil), forCellWithReuseIdentifier: kNormalCellID)
-        
+        collectionView.register(UINib(nibName: "CollectionBeautifulCell", bundle: nil), forCellWithReuseIdentifier: kBueautifulCellID)
         
         //注册Header
 //         collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: kNormalHeaderID)
@@ -91,16 +93,30 @@ extension RecommendViewController:UICollectionViewDataSource {
         }
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kBueautifulCellID, for: indexPath)
+            return cell
+        }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNormalCellID, for: indexPath)
-//        cell.backgroundColor = UIColor.green
         return cell
         
     }
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kNormalHeaderID, for: indexPath)
-//        headerView.backgroundColor = UIColor.white
-        
         return headerView
     }
     
+}
+
+//MARK: - UICollectionViewDelegate/UICollectionViewDelegateFlowLayout
+extension RecommendViewController:UICollectionViewDelegateFlowLayout,UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 1 {
+            return CGSize(width: kItemW, height: kBueautifulItemH)
+        }else{
+            return CGSize(width: kItemW, height: kNormalItemH)
+        }
+    }
+    
+
 }
---
+++ DouYuZhiBo_new/DouYuZhiBo_new/Classes/Main/View/CollectionBeautifulCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionBeautifulCell.swift
+//  DouYuZhiBo_new
+//
+//  Created by LEPU on 2020/7/18.
+//  Copyright © 2020 LEPU. All rights reserved.
+//
+
+import UIKit
+
+class CollectionBeautifulCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+    
+    
+
+}
